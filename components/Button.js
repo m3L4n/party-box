52789d5aab8e5b51a8f3dd75f208ffa02fb209ff
Julien Richard <45456851+jurichar@users.noreply.github.com>
2024-01-07 15:10:44 +0100

    Refactor Button component and add styles

--- components/Button.js
+++ components/Button.js
@@ -1,15 +1,35 @@
 // components/Button.js
 
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ title, onPress }) => {
+const buttonStyles = StyleSheet.create({
+    container: {
+        width: 200,
+        height: 50,
+        justifyContent: 'center',
+        marginVertical: 10,
+        borderRadius: 10,
+        borderStyle: 'solid',
+        borderRightWidth: 4,
+        borderBottomWidth: 4,
+        borderWidth: 2,
+        borderColor: 'black',
+    },
+    text: {
+        color: 'black',
+        textAlign: 'center',
+    },
+});
+
+
+const Button = ({ color, title, onPress }) => {
     return (
         <TouchableOpacity
-            style={{ backgroundColor: 'blue', padding: 10, marginVertical: 10 }}
+            style={{ ...buttonStyles.container, backgroundColor: color }}
             onPress={onPress}
         >
-            <Text style={{ color: 'white', textAlign: 'center' }}>{title}</Text>
+            <Text style={buttonStyles.text}>{title}</Text>
         </TouchableOpacity>
     );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,33 @@
 // src/screens/HomeScreen.js
 
 import React from 'react';
-import { Button, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from '../../components/Button';
+const homeStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(255, 198, 255)',
+    },
+});
+
+// colors :
+// (246,222,0)
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Button title="Play Fast" onPress={() => navigation.navigate('PlayFast')} />
+        <View style={homeStyles.container}>
+            <Text>Bouar</Text>
+            {/* <Button color="rgb(255, 255, 252)" title="Play Fast" onPress={() => navigation.navigate('PlayFast')} /> */}
+            {/* <Button color="rgb(255, 198, 255)" title="Partie custom" onPress={() => navigation.navigate('PlayFast')} /> */}
+            {/* <Button color="rgb(189, 178, 255)" title="Test" onPress={() => navigation.navigate('Test')} /> */}
+            {/* <Button color="rgb(160, 196, 255)" title="Test2" onPress={() => navigation.navigate('Test2')} /> */}
+            {/* <Button color="rgb(155, 246, 255)" title="Test3" onPress={() => navigation.navigate('Test3')} /> */}
+            <Button color="rgb(202, 255, 191)" title="Test4" onPress={() => navigation.navigate('Test4')} />
+            <Button color="rgb(253, 255, 182)" title="Test4" onPress={() => navigation.navigate('Test4')} />
+            {/* <Button color="rgb(255, 214, 165)" title="Test4" onPress={() => navigation.navigate('Test4')} /> */}
+            {/* <Button color="rgb(255, 173, 173)" title="Test4" onPress={() => navigation.navigate('Test4')} /> */}
         </View>
     );
 };
