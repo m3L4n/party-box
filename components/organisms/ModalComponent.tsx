50d45933c4a0e32e0bcd7443ea8ab1013c0c83b1
Julien Richard <ju@MacBook-Air-de-Julien-3.local>
2024-06-15 02:33:13 +0200

    refactor: Update background color in ModalComponent

--- components/organisms/ModalComponent.tsx
+++ components/organisms/ModalComponent.tsx
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.secondary.pink,
+    backgroundColor: colors.secondary.creme,
     margin: 30,
     borderRadius: 10,
     borderStyle: 'solid',
